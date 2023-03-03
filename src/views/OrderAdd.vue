7ee90c8648c367acd3ca42cf0eb083085491014d
Boris Fritscher <boris@fritscher.ch>
2023-03-03 10:27:43 +0100

    initial commit

---
+++ src/views/OrderAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Add new order</h2>
+
+
+  <button @click="add">Save</button>
+  <router-link to="/"><button>Cancel</button></router-link>
+</template>
+
+<script>
+import { nanoid } from "nanoid"
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    add() {
+
+
+
+      this.$router.push("/")
+    },
+    newId() {
+      return nanoid()
+    }
+  }
+}
+</script>
---
+++ src/views/OrdersView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <router-link to="add"><button>Add new order</button></router-link>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
